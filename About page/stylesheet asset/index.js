48c8ec4e7eb756b778b798114a25e02e34e1dd66
Delaine22 <108828746+Delaine22@users.noreply.github.com>
2023-03-01 11:40:07 +0100

    first commit

---
+++ About page/stylesheet asset/index.js
@@ -0,0 +1,5 @@
+var checar = window.document.getElementById('checkbtn')
+var p = window.document.getElementById('t&c')
+function check(){
+p.innerHTML='I agreed with the <a href="#">Terms & Conditions</a>'
+}
